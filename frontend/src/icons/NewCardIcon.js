06d81daa41d29437da630e631f55ee0d58c07900
Radi Cho <radicho123@gmail.com>
2019-03-17 10:19:52 +0200

    Add icons for the main menu.

---
+++ frontend/src/icons/NewCardIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NewCardIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-5 -5 30 30">
+      <path
+        d="M9 10v-2h2v2h2v2h-2v2h-2v-2h-2v-2h2zM0 3c0-1.1 0.9-2 2-2h16c1.105 0 2 0.895 2 2v0 14c0 1.105-0.895 2-2 2v0h-16c-1.105 0-2-0.895-2-2v0-14zM2 5v12h16v-12h-16z"
+        fill="#fff"
+        stroke="#068586"
+        strokeWidth="0.5"
+      />
+    </svg>
+  );
+};
+
+export default NewCardIcon;
