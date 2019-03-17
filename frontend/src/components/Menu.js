6bb7934b66f44b7eb1d212c9427f99455f0875f9
Radi Cho <radicho123@gmail.com>
2019-03-17 10:20:58 +0200

    Use the icons in the Menu. Create CSS styles for it.

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CommentIcon from "../icons/CommentIcon";
+import NewCardIcon from "../icons/NewCardIcon";
+import TimerIcon from "../icons/TimerIcon";
+import "../styles/Menu.css";
 
 const Menu = () => {
   return (
-    <div>
-      <div><Link to="/boards/card">New Card</Link></div>
-      <div><Link to="/boards/comment">Comment</Link></div>
-      <div><Link to="/timer">Timer</Link></div>
+    <div className="menu-container">
+      <Link to="/boards/card">
+        <div>
+          <NewCardIcon />
+        </div>
+      </Link>
+      <Link to="/boards/comment">
+        <div>
+          <CommentIcon />
+        </div>
+      </Link>
+      <Link to="/timer">
+        <div>
+          <TimerIcon />
+        </div>
+      </Link>
     </div>
   );
 };
